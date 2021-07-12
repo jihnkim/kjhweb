f8b66934e99196fa106d7dde3f2e3f548908c8f0
kjh <sysanal1697@gmail.com>
2021-07-12 10:12:44 +0900

    7/12 httpresponseredirect

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,12 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-    if request.method == 'POST':
+    if request.method == "POST":
 
         temp = request.POST.get('hello_world_input')
 
@@ -14,10 +16,7 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
-
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
