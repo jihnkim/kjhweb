ff1e0c8c7f9d0525cb0e068ecb61dbdae993c58b
kjh <sysanal1697@gmail.com>
2021-07-15 10:46:28 +0900

    7/15 login, logout update bootstrap load fonts update detailview update

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from accountapp.views import hello_world, AccountCreateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView
 
 app_name = 'accountapp'
 
@@ -14,5 +14,7 @@ urlpatterns = [
          LoginView.as_view(template_name='accountapp/login.html'),
          name='login'),
 
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accountapp.models import HelloWorld
 """
@@ -45,4 +45,7 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world') # 클래스형 뷰에서는 reverse_lazy 사용
     template_name = 'accountapp/create.html'
 
-
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
