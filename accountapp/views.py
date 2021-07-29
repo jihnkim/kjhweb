7828fff0e74ab893d432744dd8fc6b95dd01a711
kjh <sysanal1697@gmail.com>
2021-07-29 09:38:56 +0900

    0727 decorator

--- accountapp/views.py
+++ accountapp/views.py
@@ -62,13 +62,14 @@ has_ownership = [login_required, account_ownership_required]
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
-
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 @method_decorator(has_ownership, 'get')
