7765e5d84e1f29f2e1c1e13d868223b6919c3d70
kjh <sysanal1697@gmail.com>
2021-08-02 10:20:32 +0900

    0802 js file code update

--- static/js/magicgrid.js
+++ static/js/magicgrid.js
@@ -282,4 +282,13 @@ let magicGrid = new MagicGrid({
   useMin: true
 });
 
+
+var masonrys = document.getElementsByTagName("img")
+
+for (let i = 0; i < masonrys.length;i++) {
+  masonrys[i].addEventListener('load', function (){
+    magicGrid.positionItems();
+  }, false)
+}
+
 magicGrid.listen();
